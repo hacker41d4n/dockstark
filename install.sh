902558624d0c70780dda80bf1bc5e8a59901a8a2
hacker41d4n <54631543+hacker41d4n@users.noreply.github.com>
2022-09-26 15:49:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 
 ## Heimdall 
 
-cd containers/ && sudo heimdall-docker-compose up -d 
+cd containers && sudo heimdall-docker-compose up -d 
 
 ## Pi-Hole
 
-cd containers/ && sudo pihole-docker-compose up -d 
+cd containers && sudo pihole-docker-compose up -d 
 
 ## Portainer
 
-cd containers/ && sudo portainer-docker-compose up -d
+cd containers && sudo portainer-docker-compose up -d
 
 ## Wireguard
 
-cd containers/ && sudo wireguard-docker-compose up -d
+cd containers && sudo wireguard-docker-compose up -d
 
 ## Yacht
 
-cd containers/ && sudo yacht-docker-compose up -d
+cd containers && sudo yacht-docker-compose up -d
 
