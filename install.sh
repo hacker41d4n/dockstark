ac6a5501f200249196e5482e0ec5d222cfb62b33
hacker41d4n <54631543+hacker41d4n@users.noreply.github.com>
2022-09-26 16:35:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,23 +25,7 @@ sudo service docker start
 sudo service docker status
 
 
-## Heimdall 
+## dockstark compose
 
-cd containers && sudo heimdall-docker-compose up -d 
-
-## Pi-Hole
-
-cd containers && sudo pihole-docker-compose up -d 
-
-## Portainer
-
-cd containers && sudo portainer-docker-compose up -d
-
-## Wireguard
-
-cd containers && sudo wireguard-docker-compose up -d
-
-## Yacht
-
-cd containers && sudo yacht-docker-compose up -d
+docker-compose up -d 
 
