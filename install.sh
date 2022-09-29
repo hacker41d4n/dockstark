a966e4334e662cf6bfce243a3749945422bfc996
hacker41d4n <54631543+hacker41d4n@users.noreply.github.com>
2022-09-29 19:25:36 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -16,8 +16,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-
+sudo apt-get update -y 
 sudo apt-get install docker.io -y 
 sudo apt-get install docker-compose -y 
 sudo service docker start
